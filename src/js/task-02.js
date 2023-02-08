36de5dd021e79290502fcb5822dfd828b504fa80
Andrii Slabkyi <aslabkyi@gmail.com>
2023-02-08 22:08:56 +0200

    Complete 2 task

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,20 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const listEl = document.querySelector('ul#ingredients')
+const result = []
+
+ingredients.map((ingredient) => {
+  const itemEl = document.createElement("li");
+  itemEl.classList.add("item");
+  itemEl.textContent = ingredient;
+  result.push(itemEl);
+});
+
+listEl.append(...result)
