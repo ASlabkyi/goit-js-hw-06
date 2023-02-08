f5885b2a50eed1a7b01efa7851121d40003455d9
Andrii Slabkyi <aslabkyi@gmail.com>
2023-02-08 20:57:10 +0200

    Update task-01.js

---
+++ src/js/task-01.js
@@ -0,0 +1,9 @@
+const listEl = document.querySelector("#categories");
+const itemsEl = [...listEl.children];
+
+console.log(`Number of categories: ${itemsEl.length}`);
+
+  itemsEl.forEach((item) => {
+    console.log(item.firstElementChild.innerText);
+    console.log([...item.lastElementChild.children].length);
+  });
