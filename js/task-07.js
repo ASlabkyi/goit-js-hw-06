7ea1d589006505663bc81bc9484100d49e8c3cb5
Andrii Slabkyi <aslabkyi@gmail.com>
2023-02-12 00:54:44 +0200

    Complete task 9

--- js/task-07.js
+++ js/task-07.js
@@ -1,8 +1,6 @@
 const fontSizeControl = document.querySelector('#font-size-control')
 const textEl = document.querySelector('#text')
 
-console.log(textEl);
-
 fontSizeControl.addEventListener('input', (event) => {
     textEl.style.fontSize = `${event.currentTarget.value}px`;
 })
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
+const bodyEl = document.querySelector('body')
+const textColorEl = document.querySelector('.color')
+const colorChangeBtnEl = document.querySelector('.change-color')
+
+colorChangeBtnEl.addEventListener('click', () => {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  textColorEl.textContent = getRandomHexColor();
+})
