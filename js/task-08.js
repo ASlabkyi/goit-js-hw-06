4880b6789c2575e46e02e9cd31e09c02b0cdcc6d
Andrii Slabkyi <aslabkyi@gmail.com>
2023-02-12 00:42:46 +0200

    Complete task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const fomrEl = document.querySelector(".login-form");
 
+fomrEl.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const {
+    elements: { password, email },
+  } = event.currentTarget;
+  const data = {};
+
+  if (password.value === "" || email.value === "") {
+    alert("Всі поля повинні бути заповнені!");
+  } else {
+    data.email = email.value;
+    data.password = password.value;
+    console.log(data);
+  }
+  event.currentTarget.reset();
+}
